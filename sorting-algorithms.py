ef6f784bff7ea9bb2e41b779cd8db74f1ffbe897
Far3000-YT <far3000yt@gmail.com>
2024-11-05 09:51:43 +0100

    base program, need to add the algorithms and visuals + sounds

--- sorting-algorithms.py
+++ sorting-algorithms.py
@@ -1,12 +1,41 @@
-grid_size = 9 #test test 2 to try
-block_size = 3
-print("haha you got pranked")
+import random
 
-for block_row in range(0, 9, block_size):
-    for block_col in range(0, 9, block_size):
-        for row in range(3):
-            for col in range(3):
-                actual_row = block_row + row
-                actual_col = block_col + col
-                print(f"[{actual_row}, {actual_col}]")
+#generate random list from 1 to 1000 not in order
+numbers = list(range(1, 1001))
+random.shuffle(numbers)
 
+print("""
+WELCOME ! Choose the sorting algorithm you want to see : 
+    [1] - SELECTION SORT
+    [2] - INSERTION SORT
+    [3] - BINARY INSERTION SORT
+    [4] - MERGE SORT
+    [5] - QUICK SORT (LR ptrs)
+""")
+
+
+############################################################################################################
+
+
+def selection_sort(numbers: int):
+    pass
+
+
+############################################################################################################
+
+
+x = input("Choice : ")
+
+match x:
+    case "1":
+        selection_sort(numbers)
+    case "2":
+        print("2")
+    case "3":
+        pass
+    case "4":
+        pass
+    case "5":
+        pass
+    case _:
+        print("Please put a valid input !")
