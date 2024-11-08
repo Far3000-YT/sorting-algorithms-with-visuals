bd984521c62e316376d8e47cb66be73af3bb8362
Far3000-YT <far3000yt@gmail.com>
2024-11-08 13:57:35 +0100

    finished selection sort, adding manim to render the algorithms

--- sorting-algorithms.py
+++ sorting-algorithms.py
@@ -1,7 +1,7 @@
 import random
 
-#generate random list from 1 to 1000 not in order
-numbers = list(range(1, 11))
+#generate random list from 1 to x not in order
+numbers = list(range(1, 1001))
 random.shuffle(numbers)
 
 #base text for input
@@ -18,25 +18,30 @@ WELCOME ! Choose the sorting algorithm you want to see :
 ############################################################################################################
 
 
-#algorithms start (no explanation on how they work it's too long)
+#algorithms start
 def selection_sort(num: list):
+    print(num)
     start_index = 0
     index = 1
     min_value = numbers[start_index]
     #iterate through the list until we hit the last index (so the list will be sorted when that happens)
     while start_index < len(num):
         min_value = min(min_value, num[index])
-        if index == len(num) - 1:
-            print(start_index, num.index(min_value))
-            
+        if index == len(num) - 1:            
             #swap the 2 indexes in the list
-            num[start_index], num[num.index(min_value)] = num[num.index(min_value)], num[start_index]
+            num[num.index(min_value)], num[start_index] = num[start_index], num[num.index(min_value)]
 
             start_index += 1
             index = start_index
+            
+            try: min_value = num[start_index]
+            except Exception: pass #bcs we will go out of range and i dont want to debut this
+
         else:
             index += 1
-        print(num)
+
+    print(num)
+
 
 
 ############################################################################################################
@@ -48,7 +53,7 @@ match x:
     case "1":
         selection_sort(numbers)
     case "2":
-        print("2")
+        pass
     case "3":
         pass
     case "4":
